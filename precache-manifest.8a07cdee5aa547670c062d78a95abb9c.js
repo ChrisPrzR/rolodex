4fb4e0fec8cf838e8b044ce0a5256b97e49ae706
Christian <christian1751@gmail.com>
2020-04-09 17:57:23 -0500

    Updates

---
+++ precache-manifest.8a07cdee5aa547670c062d78a95abb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f9aa3cfebaaf50f3c3e9ff2d450453",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "48effa22406cdc3f4d4a",
+    "url": "/rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48effa22406cdc3f4d4a",
+    "url": "/rolodex/static/js/main.5250763b.chunk.js"
+  },
+  {
+    "revision": "b9c1ef4568b9e0384e14",
+    "url": "/rolodex/static/js/runtime-main.74e21264.js"
+  }
+]);
